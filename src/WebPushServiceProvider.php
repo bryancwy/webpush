6e9f3924ab8dfc13d6ca383ab7c2aef3ee3080b0
Bryan C <bryan.cwy@unipin.com>
2019-04-24 13:21:35 +0700

    first commit

---
+++ src/WebPushServiceProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace NotificationChannels\WebPush;
+
+use Illuminate\Support\Str;
+use Minishlink\WebPush\WebPush;
+use Illuminate\Support\ServiceProvider;
+
+class WebPushServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands([VapidKeysGenerateCommand::class]);
+
+        $this->mergeConfigFrom(__DIR__.'/../config/webpush.php', 'webpush');
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->when(WebPushChannel::class)
+            ->needs(WebPush::class)
+            ->give(function () {
+                return new WebPush($this->webPushConfig());
+            });
+    }
+
+    /**
+     * @return array
+     */
+    protected function webPushConfig()
+    {
+        $config = [];
+        $webpush = config('webpush');
+        $publicKey = $webpush['vapid']['public_key'];
+        $privateKey = $webpush['vapid']['private_key'];
+
+        if (! empty($webpush['gcm']['key'])) {
+            $config['GCM'] = $webpush['gcm']['key'];
+        }
+
+        if (empty($publicKey) || empty($privateKey)) {
+            return $config;
+        }
+
+        $config['VAPID'] = compact('publicKey', 'privateKey');
+        $config['VAPID']['subject'] = $webpush['vapid']['subject'];
+
+        if (empty($config['VAPID']['subject'])) {
+            $config['VAPID']['subject'] = url('/');
+        }
+
+        if (! empty($webpush['vapid']['pem_file'])) {
+            $config['VAPID']['pemFile'] = $webpush['vapid']['pem_file'];
+
+            if (Str::startsWith($config['VAPID']['pemFile'], 'storage')) {
+                $config['VAPID']['pemFile'] = base_path($config['VAPID']['pemFile']);
+            }
+        }
+
+        return $config;
+    }
+}
